81c914fdbf95ade300f764e1400e1d5234c0e41f
Damaris Canales <damariscanales00@gmail.com>
2022-06-06 19:23:08 -0400

    Local Storage Code

--- script.js
+++ script.js
@@ -12,30 +12,48 @@ $(document).ready(function () {
     localStorage.setItem(timeStamp, textContent);
   });
 
+  // Time Block Coloring Loop//
 
+  function timeColor() {
+    var currentHour = moment().hours();
+    console.log(currentHour)
 
-// Time Block Coloring Loop//
+    $(".timeBlock").each(function () {
+      //string value conversion //
+      var hourBlock = parseInt($(this).attr("id"));
 
-  function timeColor (){
-      var currentHour = moment().hours()
-      console.log(currentHour)
-      $(".timeBlock").each(function (){
-          //string value conversion //
-          var hourBlock = parseInt ($(this).attr("id"))
-          if (hourBLock < currentHour){
-              $(this).addClass("past")
-          } else if (hourBlock === currentHour) {
-            $(this).removeClass("past")
-            $(this).addClass("present")
-          } else { 
-              $(this).removeClass("past")
-              $(this).removeClass("present")
-              $(this).addClass("future")
+      if (hourBlock < currentHour) {
+        $(this).addClass("past");
+      } else if (hourBlock === currentHour) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    });
+  }
+  timeColor();
 
-          }
-      })
-  } 
-  timeColor()
+$("#9 .description").val(localStorage.getItem("9"))
+
+$("#10 .description").val(localStorage.getItem("10"))
+
+$("#11 .description").val(localStorage.getItem("11"))
+
+$("#12 .description").val(localStorage.getItem("12"))
+
+$("#13 .description").val(localStorage.getItem("13"))
+
+$("#14 .description").val(localStorage.getItem("14"))
+
+$("#15 .description").val(localStorage.getItem("15"))
+
+$("#16 .description").val(localStorage.getItem("16"))
+
+$("#17 .description").val(localStorage.getItem("17"))
+
 
 
 });
